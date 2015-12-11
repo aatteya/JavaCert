c53d11a882571ab78be90f4a09ee76993511767d
AhmedAtteya <AhmedAtteya@IBM420-PBN3D4G>
2015-12-11 09:54:03 +0200

    Print Statement From Laptop

--- JavaCert/src/com/ibm/javacert/packageTest2/GitTesting.java
+++ JavaCert/src/com/ibm/javacert/packageTest2/GitTesting.java
@@ -1,5 +1,10 @@
 package com.ibm.javacert.packageTest2;
 
 public class GitTesting extends Animals {
+	
+	//Some testing
+	public void AnimalStuff(){
+		System.out.println("This print was Developed on Laptop");
+	}
 
 }
