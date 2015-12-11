ac176c4420bdf15d1d6669ca6ee74b1599ad51e1
aatteya <ahmed.atteya90@gmail.com>
2015-12-11 10:04:10 +0200

    Adding print statement from PC

--- JavaCert/src/com/ibm/javacert/packageTest2/GitTesting.java
+++ JavaCert/src/com/ibm/javacert/packageTest2/GitTesting.java
@@ -1,5 +1,7 @@
 package com.ibm.javacert.packageTest2;
 
 public class GitTesting extends Animals {
-
+		public void nonAnimalStuff(){
+			System.out.println("This print statement was developed on PC");
+		}
 }
